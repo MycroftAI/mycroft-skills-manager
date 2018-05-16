379f09a5b798a65a72d7fadf8eaeb0eea16c51bb
Matthew D. Scholefield <matthew331199@gmail.com>
2018-05-16 15:58:20 -0500

    Increment version to 0.5.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='msm',
-    version='0.5.8',
+    version='0.5.9',
     packages=['msm'],
     install_requires=['GitPython', 'typing'],
     url='https://github.com/MycroftAI/mycroft-skills-manager',
