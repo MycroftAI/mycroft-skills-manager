4175fa05a02549279bf2976751db81de0859700f
Matthew D. Scholefield <matthew331199@gmail.com>
2019-02-13 14:50:41 -0600

    Increment version to 0.7.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='msm',
-    version='0.7.1',
+    version='0.7.2',
     packages=['msm'],
     install_requires=['GitPython', 'typing', 'fasteners', 'pyyaml', 'pako',
                       'lazy'],
