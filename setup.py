e09ef06343e22323ae7dd01057b828a6b903409c
Åke Forslund <ake.forslund@gmail.com>
2019-07-18 14:21:46 +0200

    Bump version to 0.7.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='msm',
-    version='0.7.8',
+    version='0.7.9',
     packages=['msm'],
     install_requires=[
         'GitPython', 'typing', 'fasteners', 'pyyaml', 'pako',
